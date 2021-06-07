02b2adb379f8e88c52287e3edd5a4acdf92616d7
tc9993 <taylorbcoffee@gmail.com>
2021-06-07 12:38:06 -0500

    del 3, resources

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,22 @@ total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = med
 
 # lot summary of suspension coil
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI), .groups='keep')
+
+#End of Deliverable 2-----------------------------------------------------------
+
+# t.test total summary
+t.test(suspension_coil_df$PSI, mu=1500)
+
+# Lot 1
+lot1 = subset(suspension_coil_df, Manufacturing_Lot == 'Lot1')
+t.test(lot1$PSI, mu=1500)
+
+# Lot 2
+lot2 = subset(suspension_coil_df, Manufacturing_Lot == 'Lot2')
+t.test(lot2$PSI, mu=1500)
+
+# Lot 3
+lot3 = subset(suspension_coil_df, Manufacturing_Lot == 'Lot3')
+t.test(lot3$PSI, mu=1500)
+
+#End of Deliverable 3-----------------------------------------------------------
