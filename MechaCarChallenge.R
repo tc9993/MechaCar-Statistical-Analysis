13e2290c997e5eceff5274540eed1eaf5f345c3a
tc9993 <taylorbcoffee@gmail.com>
2021-06-07 11:53:16 -0500

    push 3, Del 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,14 @@ linear_regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
 
 # perform summary
 summary(linear_regression)
+
+#End of Deliverable 1-----------------------------------------------------------
+
+# read suspension coil file
+suspension_coil_df = read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
+
+# total summary of suspension coil
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
+
+# lot summary of suspension coil
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI), .groups='keep')
